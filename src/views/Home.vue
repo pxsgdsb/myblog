4c6fc1e07f2e604ab67ea985f0109c03567221e8
pxsgdsb <1851345097@qq.com>
2021-08-16 18:00:38 +0800

    上传

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <div class="glass">
+        <h1>HELLO WORLD</h1>
+      </div>
+    </header>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
+<style lang="less" scoped>
+  header{
+    width: 100vw;
+    height: 100vh;
+    background: url("../assets/image/headerbg2.jpg");
+    background-size:cover;
+    .glass{
+      width: 100%;
+      height: 100%;
+      background: rgba(08,08,08,0.4);
+      box-shadow:inset 5px 5px 40px 20px #080808;
+      animation: outrgba 2s ease 0s;
+      animation-fill-mode: forwards;
+      display: flex;
+      align-items: center;
+      h1{
+        text-align: center;
+        color: white;
+        width: 100%;
+        font-size: 50px;
+      }
+      &:hover{
+        animation: inrgba 2s ease 0s;
+        animation-fill-mode: forwards;
+      }
+      @keyframes inrgba{
+          from {background: rgba(08,08,08,0.3);}
+          to {background: rgba(08,08,08,.0);}
+      }
+      @keyframes outrgba{
+          from {background: rgba(08,08,08,0);}
+          to {background: rgba(08,08,08,.0.3);}
+      }
+    }
+  }
+</style>
